93d5fc9ca632c17d621584b698fbe87157481cc9
ScottENOC <bales.scott@gmail.com>
2026-09-23 19:15:21 +1000

    Load shared siege reserve dispatch

--- renderPerfTuning.js
+++ renderPerfTuning.js
@@ -72,6 +72,7 @@
     load('script[data-siege-sector-system]', 'siegeSectorSystem.js', 'siegeSectorSystem');
     load('script[data-siege-actor-sector-integration]', 'siegeActorSectorIntegration.js', 'siegeActorSectorIntegration');
     load('script[data-siege-actor-reconciliation]', 'siegeActorReconciliation.js', 'siegeActorReconciliation');
+    load('script[data-siege-reserve-dispatch]', 'siegeReserveDispatch.js', 'siegeReserveDispatch');
 
     function installVisibilityBounds() {
         const original = window.isVisibleToPlayer;
