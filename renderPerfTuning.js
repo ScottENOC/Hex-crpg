5ca1353c005ecedb4205fcb5be9b3dfdb84e6550
ScottENOC <bales.scott@gmail.com>
2026-09-24 08:54:00 +1000

    Load personality-aware ambient chatter

--- renderPerfTuning.js
+++ renderPerfTuning.js
@@ -64,6 +64,7 @@
     load('script[data-emberlode-expansion]', 'emberlodeExpansion.js', 'emberlodeExpansion');
     load('script[data-reactive-ambient-chatter]', 'ambientChatterExpansion.js', 'reactiveAmbientChatter');
     load('script[data-relationship-ambient-chatter]', 'ambientChatterRelationships.js', 'relationshipAmbientChatter');
+    load('script[data-personality-ambient-chatter]', 'ambientChatterPersonality.js', 'personalityAmbientChatter');
     load('script[data-silverhart-capital-rebuild]', 'silverhartCapitalRebuild.js', 'silverhartCapitalRebuild');
     load('script[data-silverhart-capital-completion]', 'silverhartCapitalCompletion.js', 'silverhartCapitalCompletion');
     load('script[data-silverhart-avenue-detours]', 'silverhartAvenueDetours.js', 'silverhartAvenueDetours');
