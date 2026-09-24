9b462f2535ceefb2bf257031d71668c3ad30df36
ScottENOC <bales.scott@gmail.com>
2026-09-24 11:03:17 +1000

    Load faction belief layer for wilderness incidents

--- renderPerfTuning.js
+++ renderPerfTuning.js
@@ -68,6 +68,7 @@
     load('script[data-bandit-camp-wilderness-fix]', 'banditCampWildernessFix.js', 'banditCampWildernessFix');
     load('script[data-wilderness-incidents]', 'wildernessIncidents.js', 'wildernessIncidents');
     load('script[data-wilderness-consequences]', 'wildernessConsequences.js', 'wildernessConsequences');
+    load('script[data-wilderness-faction-beliefs]', 'wildernessFactionBeliefs.js', 'wildernessFactionBeliefs');
     load('script[data-silverhart-capital-rebuild]', 'silverhartCapitalRebuild.js', 'silverhartCapitalRebuild');
     load('script[data-silverhart-capital-completion]', 'silverhartCapitalCompletion.js', 'silverhartCapitalCompletion');
     load('script[data-silverhart-avenue-detours]', 'silverhartAvenueDetours.js', 'silverhartAvenueDetours');
